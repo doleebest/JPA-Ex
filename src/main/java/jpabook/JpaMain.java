fd5b3ce99bfbe1b9174ad0bced02c6416e6c5116
Sojeong Lee <sj27419@gmail.com>
2026-09-13 22:58:13 +0900

    feature: ex1

---
+++ src/main/java/jpabook/JpaMain.java
@@ -0,0 +1,25 @@
+package jpabook;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Order;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        }
+
+        em.close();
+        emf.close();
+    }
+}
